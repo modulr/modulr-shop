cc4452ee9900908f276432a249017a513cdcfb24
alfredobarron <alfredobarronc@gmail.com>
2023-06-29 11:52:36 -0600

    add header and improve styles

--- src/views/Autoparts.vue
+++ src/views/Autoparts.vue
@@ -1,6 +1,7 @@
 <script setup>
     import { onMounted, computed, ref } from 'vue'
     import Multiselect from '@vueform/multiselect'
+    import Header from '../components/Header.vue';
     import Footer from '../components/Footer.vue';
     import { useAutopartsStore } from '../stores/autoparts'; 
     import { register } from 'swiper/element/bundle';
@@ -110,9 +111,7 @@
 <template>
     <div class="bg-[url('/img/bg-hero.jpg')] bg-no-repeat bg-top bg-cover w-full h-full">
         <div class="container mx-auto px-4">
-            <div class="flex justify-end mt-4">
-                <router-link to="/login" class="py-2 px-12 font-medium text-red-600 border border-red-600 rounded-full hover:bg-red-600 hover:border-red-600 hover:text-white">Entrar</router-link>
-            </div>
+            <Header class="mt-6"></Header>
             <div class="flex flex-wrap pt-20 pb-32 lg:pt-32 lg:pb-40 xl:pt-46 xl:pb-60 2xl:pt-52 2xl:pb-72">
                 <div class="mb-8 pl-1">
                     <h1 class="font-title font-bold text-4xl text-gray-900 md:text-5xl drop-shadow-md">Auto <span class="text-red-600">Global</span></h1>
