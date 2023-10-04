e98a898286d5f311ae35029ec62004e7c2ff8aff
Alfredo Barron <alfredobarronc@gmail.com>
2023-10-04 16:37:35 -0600

    Autopart -> add condition, position, side

--- src/views/Autopart.vue
+++ src/views/Autopart.vue
@@ -70,7 +70,7 @@
                         <div class="flex items-center sticky top-10 z-50 overflow-hidden">
                             <div class="w-full mb-14">
                                 <div class="flex space-x-6">
-                                    <span class="text-sm font-medium" :class="[autopartsStore.autopart.origin_id == 1 ? 'text-red-600' : 'text-gray-400']">{{ autopartsStore.autopart.origin?.name }}</span>
+                                    <span class="text-sm font-medium" :class="[autopartsStore.autopart.condition_id == 1 ? 'text-red-600' : 'text-gray-400']">{{ autopartsStore.autopart.condition?.name }}</span>
                                     <span class="flex items-center">
                                         <svg v-for="quality in autopartsStore.autopart.quality" :key="quality" class="block h-4 w-4 align-middle text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" class=""></path>
@@ -110,13 +110,24 @@
                                         Mercado Libre
                                     </a>
                                 </div>
-                                <div class="space-y-2">
-                                    <p class="text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.store"><span class="font-light">Vendedor.</span> {{ autopartsStore.autopart.store?.name }} <span v-if="autopartsStore.autopart.store_ml"> | {{ autopartsStore.autopart.store_ml?.name }}</span></p>
-                                    <p class="text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.ml_id"><span class="font-light">ML.</span> {{ autopartsStore.autopart.ml_id }}</p>
-                                    <p class="text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.autopart_number"><span class="font-light">No. parte.</span> {{ autopartsStore.autopart.autopart_number }}</p>
-                                    <p class="text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.id"><span class="font-light">ID.</span> {{ autopartsStore.autopart.id }}</p>
-                                    <p class="text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.location"><span class="font-light">Ubicación.</span> {{ autopartsStore.autopart.location }}</p>
+
+                                <div class="bg-gray-100 dark:bg-gray-700 rounded-xl">
+                                    <div class="p-3 lg:p-5">
+                                        <div class="p-2 rounded-xl lg:p-6 dark:bg-gray-800 bg-gray-50">
+                                            <div class="flex flex-wrap justify-center gap-y-4">
+                                                <p class="w-full text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.store"><span class="font-light"></span> {{ autopartsStore.autopart.store?.name }} <span v-if="autopartsStore.autopart.store_ml"> | {{ autopartsStore.autopart.store_ml?.name }}</span></p>
+                                                <p class="w-full text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.ml_id"><span class="font-light"></span> {{ autopartsStore.autopart.ml_id }}</p>
+                                                <p class="w-full text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.id"><span class="font-light">ID.</span> {{ autopartsStore.autopart.id }}</p>
+                                                <p class="w-full text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.autopart_number"><span class="font-light">No. parte.</span> {{ autopartsStore.autopart.autopart_number }}</p>
+                                                <p class="w-full text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.location"><span class="font-light">Ubicación.</span> {{ autopartsStore.autopart.location.name }}</p>
+                                                <p class="w-full text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.origin"><span class="font-light">Origen.</span> {{ autopartsStore.autopart.origin.name }}</p>
+                                                <p class="w-full text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.position"><span class="font-light">Posición.</span> {{ autopartsStore.autopart.position.name }}</p>
+                                                <p class="w-full text-sm font-medium text-gray-600" v-if="autopartsStore.autopart.side"><span class="font-light">Lado.</span> {{ autopartsStore.autopart.side.name }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
                     </div>
